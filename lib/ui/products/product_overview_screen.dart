f7605b7fb393a4a7f8ae30f117ad252201d2a80d
baotran9 <baotran096b@gmail.com>
2022-09-24 20:45:35 +0700

    Xay dung drawer va dinh tuyen theo ten

--- lib/ui/products/product_overview_screen.dart
+++ lib/ui/products/product_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:myshop/ui/cart/cart_screen.dart';
+import '../shared/app_drawer.dart';
 import 'products_grid.dart';
 
 enum FilterOptions { favorites, all }
@@ -23,6 +24,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           buildShoppingCartIcon(),
         ],
       ),
+      drawer: const AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
@@ -33,7 +35,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
         Icons.shopping_cart,
       ),
       onPressed: () {
-        print('Go to cart screen');
+        Navigator.of(context).pushNamed(CartScreen.routeName);
       },
     );
   }
